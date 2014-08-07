fb31f49edd3a04840da2b3cfd993949bed81d7a9
David Rees <davidarees@outlook.com>
2014-08-07 09:26:20 +0100

    event code not currently work as html location path seems to contain %3f instead of ?

--- TicTacToe/app/assets/javascripts/script.js
+++ TicTacToe/app/assets/javascripts/script.js
@@ -4,12 +4,18 @@ var myApp = {
   },
   dragAndDrop: function(){
     $('.draggable').draggable();
-    $('#droppable').droppable({
+    $('.droppable').droppable({
       drop: function(event, ui){
         $(this)
           .addClass('ui-state-highlight')
           .find('p')
           .html('Dropped');
+          moveId = $(event.target).data("cell");
+          gameId = window.gameId;
+          var path = "/tic_tac_toe_games/"+gameId+"/tic_tac_toe_moves/new?move="+moveId;
+          $(location).pathname = path;
+          alert(path);
+          alert(document.location.pathname);
       }
     })
   }
